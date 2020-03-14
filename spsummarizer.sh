b70b888169fb92e8b4a3989538fa1c28bb7b552b
verovaleros <vero.valeros@gmail.com>
2020-03-14 22:34:07 +0100

    General summary added

--- spsummarizer.sh
+++ spsummarizer.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+
 VERSION=0.1
-
-echo "Simple Pcap Analyzer. Version $VERSION. Author: Veronica Valeros (@verovaleros)."
-
-pcapfile=$1
+PCAP=$1
 
 # Verifying that parameters were given
 if [ -z "$1" ];
@@ -16,3 +14,26 @@ then
     exit
 fi
 
+
+echo "Simple Pcap Analyzer. Version $VERSION. Author: Veronica Valeros (@verovaleros)."
+echo
+
+# Obtain size of the Pcap
+PCAP_SIZE=$(du -h $PCAP |cut -f1)
+
+# Obtain number of packets
+PCAP_PKTS=$(tcpdump -nn -r $PCAP 2>/dev/null | wc -l)
+
+# Timestamp first packet: 
+T1=$(tcpdump -tt -nn -s0 -r $PCAP 2>/dev/null |head -n 1 | awk -F " IP" '{print $1}'|awk -F\. '{print $1}')
+
+# Timestamp last packet: 
+T2=$(tcpdump -tt -nn -s0 -r $PCAP 2>/dev/null |tail -n 1 | awk -F " IP" '{print $1}'|awk -F\. '{print $1}')
+
+# Calculate PCAP Duration
+PCAP_DUR=$(( ( $T2 - $T1 ) / 3600 ))
+
+
+echo "PCAP General Summary"
+echo "==============================="
+echo "Size: $PCAP_SIZE    Packets: $PCAP_PKTS    Duration: $PCAP_DUR hs"
