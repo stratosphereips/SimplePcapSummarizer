1167282312da9ce31ccea7de2648c83344fb003b
verovaleros <vero.valeros@gmail.com>
2020-11-26 18:59:48 +0100

    Check if woothee is installed

--- mod_useragent.py
+++ mod_useragent.py
@@ -1,4 +1,8 @@
-import woothee
 import sys
+try:
+    import woothee
+except ImportError as e:
+    print("Install woothee via pip3 to use this module")
+    sys.exit()
 
 print(woothee.parse(sys.argv[1])[sys.argv[2]])
