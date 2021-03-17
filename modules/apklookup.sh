e73cd1c5a1f2009f93e1c786ea6a56e033da956b
verovaleros <vero.valeros@gmail.com>
2021-03-17 10:33:19 +0100

    Added comments and header

--- modules/apklookup.sh
+++ modules/apklookup.sh
@@ -1,1 +1,3 @@
+# Author: Veronica Valeros <vero.valeros@gmail.com>
+# Given an app ID, this script obtains an app name and URL from the Google Play Store
 curl -s https://play.google.com/store/apps/details\?id\=$1 | grep "\"name\"\:" | awk -F "\"name\"\:\"" '{print $2}' |awk -F "\",\"url\"\:\"" '{print $1" ("$2}' | awk -F "\?" '{print $1")"}'
